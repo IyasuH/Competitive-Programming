15b046b5e727c9c2cc0af63404dc24508dfa4b93
IyasuH <eyasuhailegbr@gmail.com>
2023-01-10 18:43:33 +0300

    grading_student hackerrank

---
+++ gradingStudent.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    # my code starts here
+    results = []
+    for grade in grades:
+        rmend = grade%5
+        add = 5-rmend
+        if add < 3 and grade>=38:
+            results.append(grade+add)
+        else:
+            results.append(grade)
+    return results
+    # ends here
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
