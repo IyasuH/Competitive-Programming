2021a3060d71481bdad9babe9ceb608afb60877c
IyasuH <eyasuhailegbr@gmail.com>
2023-02-06 13:28:50 +0300

    kth largest Element in array

---
+++ kthLargestElementAr.py
@@ -0,0 +1,10 @@
+# 1 trial 2 min
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums.sort()
+        return(nums[-k])
